39465244229002b5d7488fade2f91ce24aa3c85f
Piyus662 <piyush.vedangdda@gmail.com>
2024-12-01 16:44:08 +0000

    variables Chapter done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail="piyush@gmail.com"
+var accountPassword = "12345"
+accountCity = 13
+accountCity = "4d4"
+//accountId = 12
+console.log(typeof accountCity);
+console.table([accountEmail,accountId,accountPassword,accountCity])
+/*avoid use var because of issue in block scope and Functional scope. 
+So use let instead of var 
+*/
